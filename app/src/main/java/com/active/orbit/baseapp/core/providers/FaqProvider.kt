400e8bf84d96b10ea6bb2fb9e2d249dfe9efe7d0
GeorgeStavrou <george.stavr94@gmail.com>
2023-08-07 17:45:50 +0300

    improved FAQs

--- app/src/main/java/com/active/orbit/baseapp/core/providers/FaqProvider.kt
+++ app/src/main/java/com/active/orbit/baseapp/core/providers/FaqProvider.kt
@@ -40,8 +40,7 @@ class FaqProvider private constructor(private val context: Context) {
         val category1 = FaqModel(context.resources.getString(R.string.faq_categories_1))
         val category1sub1 = FaqModel(context.resources.getString(R.string.faq_questions_general_1))
         val category1sub2 = FaqModel(context.resources.getString(R.string.faq_questions_general_2))
-        val category1sub3 = FaqModel(context.resources.getString(R.string.faq_questions_general_3))
-        val category1sub4 = FaqModel(context.resources.getString(R.string.faq_questions_general_4))
+        val category1sub3 = FaqModel(context.resources.getString(R.string.faq_questions_general_4))
 
         val category2 = FaqModel(context.resources.getString(R.string.faq_categories_2))
         val category2sub1 = FaqModel(context.resources.getString(R.string.faq_questions_setup_1))
@@ -54,8 +53,7 @@ class FaqProvider private constructor(private val context: Context) {
 
         val category1sub1Response = FaqModel(category1sub1.category, context.resources.getString(R.string.faq_answers_1))
         val category1sub2Response = FaqModel(category1sub2.category, context.resources.getString(R.string.faq_answers_2))
-        val category1sub3Response = FaqModel(category1sub3.category, context.resources.getString(R.string.faq_answers_3))
-        val category1sub4Response = FaqModel(category1sub4.category, context.resources.getString(R.string.faq_answers_4))
+        val category1sub3Response = FaqModel(category1sub3.category, context.resources.getString(R.string.faq_answers_4))
         val category2sub1Response = FaqModel(category2sub1.category, context.resources.getString(R.string.faq_answers_5))
         val category2sub2Response = FaqModel(category2sub2.category, context.resources.getString(R.string.faq_answers_6))
         val category2sub3Response = FaqModel(category2sub3.category, context.resources.getString(R.string.faq_answers_7))
@@ -68,26 +66,23 @@ class FaqProvider private constructor(private val context: Context) {
         category1sub1.position = 4
         category1sub2.position = 5
         category1sub3.position = 6
-        category1sub4.position = 7
-        category2sub1.position = 8
-        category2sub2.position = 9
-        category2sub3.position = 10
-        category3sub1.position = 11
-        category3sub2.position = 12
-        category1sub1Response.position = 13
-        category1sub2Response.position = 14
-        category1sub3Response.position = 15
-        category1sub4Response.position = 16
-        category2sub1Response.position = 17
-        category2sub2Response.position = 18
-        category2sub3Response.position = 19
-        category3sub1Response.position = 20
-        category3sub2Response.position = 21
+        category2sub1.position = 7
+        category2sub2.position = 8
+        category2sub3.position = 9
+        category3sub1.position = 10
+        category3sub2.position = 11
+        category1sub1Response.position = 12
+        category1sub2Response.position = 13
+        category1sub3Response.position = 14
+        category2sub1Response.position = 15
+        category2sub2Response.position = 16
+        category2sub3Response.position = 17
+        category3sub1Response.position = 18
+        category3sub2Response.position = 19
 
         category1sub1.response = category1sub1Response
         category1sub2.response = category1sub2Response
         category1sub3.response = category1sub3Response
-        category1sub4.response = category1sub4Response
         category2sub1.response = category2sub1Response
         category2sub2.response = category2sub2Response
         category2sub3.response = category2sub3Response
@@ -97,7 +92,6 @@ class FaqProvider private constructor(private val context: Context) {
         category1.subCategories.add(category1sub1)
         category1.subCategories.add(category1sub2)
         category1.subCategories.add(category1sub3)
-        category1.subCategories.add(category1sub4)
         category2.subCategories.add(category2sub1)
         category2.subCategories.add(category2sub2)
         category2.subCategories.add(category2sub3)
